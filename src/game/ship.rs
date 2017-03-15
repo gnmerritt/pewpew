dc12d5cdfb3a57db20f353e9cdee76f4ef7982d9
Nathan Merritt <gnm@the-merritts.net>
2017-03-15 11:10:33 -0400

    Ship struct & serialization test

---
+++ src/game/ship.rs
@@ -0,0 +1,31 @@
+use na::{Vector3, Rotation3};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Ship {
+    position: Vector3<i32>,
+    orientation: Rotation3<i32>,
+    velocity: Vector3<i32>,
+}
+
+#[cfg(test)]
+mod test {
+    use na::{Vector3, Rotation3};
+    use bincode::{serialize, deserialize, SizeLimit};
+    use super::Ship;
+
+    #[test]
+    fn serialization() {
+        let ship = Ship {
+            position: Vector3::new(0, 0, 0),
+            orientation: Rotation3::identity(),
+            velocity: Vector3::new(1, 0, 0),
+        };
+        let encoded: Vec<u8> = serialize(&ship, SizeLimit::Infinite).unwrap();
+
+        assert_eq!(encoded.len(), 60); // TODO: why is this 60 bytes?
+
+        let decoded: Ship = deserialize(&encoded[..]).unwrap();
+
+        assert_eq!(ship, decoded);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@ extern crate gfx;
 extern crate gfx_window_glutin;
 extern crate glutin;
 
-extern crate nalgebra;
+extern crate nalgebra as na;
 extern crate nphysics3d;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate bincode;
+
 pub mod engine;
+pub mod game;
